8e2fe6db0f52643f2e14aaab69026aa09ab76162
feler <feler@archlinux.us>
2009-07-05 15:02:26 -0700

    * Created lua/config.c and lua/config.h for lua configuration management

---
+++ src/lua/config.c
@@ -0,0 +1,69 @@
+/*
+ * config.c - lua functions for configuretion file management
+ *
+ * Copyright © 2009 Ángel Alonso (feler) <feler@archlinux.us> 
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+#include <stdio.h>
+
+#include "structs.h"
+#include "util.h"
+#include "lua/config.h"
+
+/* clua_GetNumber - get a number from a provided with provided name
+ */
+lua_Number
+clua_GetNumber(lua_State *L, const char *name)
+{
+    lua_getglobal(L, name);
+    if(!lua_isnumber(L, -1))
+        die("config: '%s' must be number", name);
+    return lua_tonumber(L, -1);
+}
+
+/* clua_Init - init lua
+ */
+void
+clua_Init(void)
+{
+    /* open a new stack */
+    rootconf.L = lua_open();
+
+    /* load base libraries */
+    luaL_openlibs(rootconf.L);
+}
+
+/* clua_ParseConfig - parse the config file getting the options
+ */
+bool
+clua_ParseConfig(void)
+{
+    if(luaL_loadfile(rootconf.L, "/home/feler/.ceresrc") ||
+       lua_pcall(rootconf.L, 0, 0, 0))
+        die("cannot run cofiguration file: %s\n", lua_tostring(rootconf.L, -1));
+
+    rootconf.config.mfact = (float)clua_GetNumber(rootconf.L, "mfact");
+
+    return true;
+}
+
+// vim:et:sw=4:ts=8:softtabstop=4:cindent:fdm=marker:tw=80
---
+++ src/lua/config.h
@@ -0,0 +1,33 @@
+/*
+ * config.h - lua config management header
+ *
+ * Copyright © 2009 Ángel Alonso (feler) <feler@archlinux.us> 
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef CLUA_CONFIG_H
+#define CLUA_CONFIG_H
+
+#include <lua.h>
+
+lua_Number clua_GetNumber(lua_State *, const char *);
+void clua_Init(void);
+bool clua_ParseConfig(void);
+
+#endif
+
+// vim:et:sw=4:ts=8:softtabstop=4:cindent:fdm=marker:tw=80
