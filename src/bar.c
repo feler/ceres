5df5116d88255d2fe0eb97d725009fce36f1708e
Àngel Alonso <feler@archlinux.us>
2009-07-11 07:49:28 +0200

    bar: 1 pixel left margin in window name displaying

--- src/bar.c
+++ src/bar.c
@@ -205,7 +205,7 @@ bar_update_task_list(bar_t *bar)
     for(i = 0, client = client_next_tiled(rootconf.clients);
         client; client = client_next_tiled(client->next), i++)
     {
-        area.x      = i == 0 ? 0 : single_width * i;
+        area.x      = (i == 0 ? 0 : single_width * i) + 1;
         area.y      = 0;
         area.width  = single_width;
         area.height = bar->geom.height;
